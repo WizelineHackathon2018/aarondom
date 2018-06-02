9177026912578493e7b4b98015b240bf5cefbe23
aarondomo <lrc_aaron@hotmail.com>
2018-06-02 15:11:54 -0400

    Add user and team layouts

---
+++ app/src/main/java/com/aarondomo/wizeline/ui/NewTeamFragment.java
@@ -0,0 +1,21 @@
+package com.aarondomo.wizeline.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.aarondomo.wizeline.R;
+
+public class NewTeamFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_new_team, container, false);
+
+        return view;
+    }
+}
