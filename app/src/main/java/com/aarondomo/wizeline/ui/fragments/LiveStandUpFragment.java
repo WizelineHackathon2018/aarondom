a455b77a396ba1550d21d7f995dd4f3eb09fa7a0
aarondomo <lrc_aaron@hotmail.com>
2018-06-02 19:08:01 -0400

    Add audio recording capacity

--- app/src/main/java/com/aarondomo/wizeline/ui/fragments/LiveStandUpFragment.java
+++ app/src/main/java/com/aarondomo/wizeline/ui/fragments/LiveStandUpFragment.java
@@ -1,20 +1,33 @@
 package com.aarondomo.wizeline.ui.fragments;
 
+import android.content.Context;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.aarondomo.wizeline.R;
+
+import java.io.IOException;
+import java.util.UUID;
 
 public class LiveStandUpFragment extends Fragment {
 
     private Button startStandUp;
     private TextView speaker;
     private TextView timer;
+
+    private Button stop, record;
+
+    private String outputFile;
+    private MediaRecorder audioMediaRecorder = new MediaRecorder();
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -27,6 +40,11 @@ public class LiveStandUpFragment extends Fragment {
         timer = view.findViewById(R.id.textview_timer);
 
         setUpStartStandUpButton();
+
+        stop = (Button) view.findViewById(R.id.stop);
+        record = (Button) view.findViewById(R.id.record);
+
+        prepareAudio();
 
         return view;
     }
@@ -41,4 +59,52 @@ public class LiveStandUpFragment extends Fragment {
         });
     }
 
+    private void prepareAudio(){
+        stop.setEnabled(false);
+        final Context applicationContext = getActivity().getApplicationContext();
+
+        record.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                outputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/recording_" + UUID.randomUUID() + ".3gp";
+                audioMediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+                audioMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+                audioMediaRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+                audioMediaRecorder.setOutputFile(outputFile);
+
+                try {
+                    audioMediaRecorder.prepare();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                audioMediaRecorder.start();
+                record.setEnabled(false);
+                stop.setEnabled(true);
+
+                Toast.makeText(applicationContext, "Recording started", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                audioMediaRecorder.stop();
+                audioMediaRecorder.reset();
+                record.setEnabled(true);
+                stop.setEnabled(false);
+                Toast.makeText(applicationContext, "Audio Recorder stopped", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (audioMediaRecorder != null){
+            audioMediaRecorder.release();
+            audioMediaRecorder = null;
+        }
+    }
 }
