fb5252bfdfb311c72ee013fdfba3c9553397de7e
aarondomo <lrc_aaron@hotmail.com>
2018-06-02 19:38:11 -0400

    Add Text to Speech

--- app/src/main/java/com/aarondomo/wizeline/ui/fragments/LiveStandUpFragment.java
+++ app/src/main/java/com/aarondomo/wizeline/ui/fragments/LiveStandUpFragment.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
+import android.speech.tts.TextToSpeech;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,9 +17,11 @@ import android.widget.Toast;
 import com.aarondomo.wizeline.R;
 
 import java.io.IOException;
+import java.util.Locale;
 import java.util.UUID;
 
-public class LiveStandUpFragment extends Fragment {
+public class LiveStandUpFragment extends Fragment implements
+        TextToSpeech.OnInitListener {
 
     private Button startStandUp;
     private TextView speaker;
@@ -28,6 +32,8 @@ public class LiveStandUpFragment extends Fragment {
     private String outputFile;
     private MediaRecorder audioMediaRecorder = new MediaRecorder();
 
+    private TextToSpeech textToSpeech;
+    private Button btnSpeak;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -41,10 +47,24 @@ public class LiveStandUpFragment extends Fragment {
 
         setUpStartStandUpButton();
 
+        //Audio Recording
         stop = (Button) view.findViewById(R.id.stop);
         record = (Button) view.findViewById(R.id.record);
 
-        prepareAudio();
+        prepareAudioRecording();
+
+        //Text to speech
+        btnSpeak = (Button) view.findViewById(R.id.btnSpk);
+        textToSpeech = new TextToSpeech(getContext(), this);
+        announceNext();
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                speakOut();
+            }
+
+        });
 
         return view;
     }
@@ -59,7 +79,7 @@ public class LiveStandUpFragment extends Fragment {
         });
     }
 
-    private void prepareAudio(){
+    private void prepareAudioRecording(){
         stop.setEnabled(false);
         final Context applicationContext = getActivity().getApplicationContext();
 
@@ -106,5 +126,41 @@ public class LiveStandUpFragment extends Fragment {
             audioMediaRecorder.release();
             audioMediaRecorder = null;
         }
+
+        if (textToSpeech != null) {
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
+    }
+
+    private void announceNext() {
+
+    }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+
+            Locale locale = new Locale("es_419");
+            int result = textToSpeech.setLanguage(locale);
+
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "Language is not supported");
+            } else {
+                btnSpeak.setEnabled(true);
+                speakOut();
+            }
+
+        } else {
+            Log.e("TTS", "Initilization Failed");
+        }
+    }
+
+    private void speakOut() {
+
+        String text = "Tu turno, Pedro ";
+
+        textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null);
     }
 }
